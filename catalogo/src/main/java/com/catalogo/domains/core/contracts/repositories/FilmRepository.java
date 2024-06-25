744b7a53443ee525c5f4c4cb7a67ca6eb3f21b39
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2024-06-25 16:59:05 +0200

    remove unnecessary interfaces

--- catalogo/src/main/java/com/catalogo/domains/core/contracts/repositories/FilmRepository.java
+++
@@ -1,7 +0,0 @@
-package com.catalogo.domains.core.contracts.repositories;
-
-import com.catalogo.domains.entities.Film;
-
-public interface FilmRepository extends ProjectionsAndSpecificationJpaRepository<Film, Integer> {
-
-}
