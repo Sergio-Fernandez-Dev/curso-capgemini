df8fd4484951aa9c93722a4da06b362737b5ce4a
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2024-06-26 16:01:30 +0200

    refactorize tests

--- catalogo/src/test/java/com/catalogo/domains/entities/FilmTest.java
+++ catalogo/src/test/java/com/catalogo/domains/entities/FilmTest.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 
