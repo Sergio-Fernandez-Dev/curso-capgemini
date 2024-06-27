dac8c287a8e5ec4f62270cd6c18d2422647c7d8f
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2024-06-27 13:27:13 +0200

    add step

--- lotes-java/src/main/java/com/example/batch/PersonasBatchConfiguration.java
+++ lotes-java/src/main/java/com/example/batch/PersonasBatchConfiguration.java
@@ -5,7 +5,12 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
@@ -14,7 +19,7 @@ import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitialization;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import com.example.models.Persona;
@@ -28,8 +33,9 @@ public class PersonasBatchConfiguration {
 	PlatformTransactionManager transactionManager;
 
 	public FlatFileItemReader<PersonaDTO> personaCSVItemReader(String fname) {
-		return new FlatFileItemReaderBuilder<PersonaDTO>().name("personaCSVItemReader")
-				.resource(new FileSystemResource(fname))
+		return new FlatFileItemReaderBuilder<PersonaDTO>()
+				.name("personaCSVItemReader")
+				.resource(new ClassPathResource(fname))
 				.linesToSkip(1)
 				.delimited()
 				.names(new String[] { "id", "nombre", "apellidos", "correo", "sexo", "ip" })
@@ -49,6 +55,25 @@ public class PersonasBatchConfiguration {
 				.itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
 				.sql("INSERT INTO personas VALUES (:id,:nombre,:correo,:ip)")
 				.dataSource(dataSource)
+				.build();
+	}
+	
+	@Bean
+	public Step importCSV2DBStep1(JdbcBatchItemWriter<Persona> personaDBItemWriter) {
+		return new StepBuilder("importCSV2DBStep1", jobRepository)
+				.<PersonaDTO, Persona>chunk(10, transactionManager)
+				.reader(personaCSVItemReader("personas-1.csv"))
+				.processor(personaItemProcessor)
+				.writer(personaDBItemWriter)
+				.build();
+	}
+
+	@Bean
+	public Job personasJob(PersonasJobListener listener, Step importCSV2DBStep1) {
+		return new JobBuilder("personasJob", jobRepository)
+				.incrementer(new RunIdIncrementer())
+				.listener(listener)
+				.start(importCSV2DBStep1)
 				.build();
 	}
 }
