8150f2a238f39ef44d6282bef806a6b5a0a4e693
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2024-07-01 11:49:05 +0200

    change path

--- demo/src/main/java/com/example/domains/contracts/service/ActorService.java
+++
@@ -1,8 +0,0 @@
-package com.example.domains.contracts.service;
-
-import com.example.domains.core.contracts.services.ProjectionDomainService;
-import com.example.domains.entities.Actor;
-
-public interface ActorService extends ProjectionDomainService<Actor, Integer> {
-	void repartePremios();
-}
