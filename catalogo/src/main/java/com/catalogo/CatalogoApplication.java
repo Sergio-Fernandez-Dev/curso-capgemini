6237f10d2cfb67a644684c094f1b1562b4d1d25d
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2024-07-02 14:22:26 +0200

    implement resources for language and category

--- catalogo/src/main/java/com/catalogo/CatalogoApplication.java
+++ catalogo/src/main/java/com/catalogo/CatalogoApplication.java
@@ -1,17 +1,12 @@
 package com.catalogo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.catalogo.domains.contracts.services.FilmService;
-
 @SpringBootApplication
 public class CatalogoApplication implements CommandLineRunner{
-	
-	@Autowired
-	FilmService service;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogoApplication.class, args);
