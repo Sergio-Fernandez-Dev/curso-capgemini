e35a5e46a81b153fb8d56dfa29fc34a04bb25272
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2024-07-05 11:10:21 +0200

    update project

--- demo/src/main/java/com/example/domains/contracts/repositories/LanguageRepository.java
+++ demo/src/main/java/com/example/domains/contracts/repositories/LanguageRepository.java
@@ -5,13 +5,9 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.example.domains.entities.Language;
-import java.util.List;
-import java.sql.Timestamp;
-
 
 @RepositoryRestResource(path="idiomas", itemResourceRel="idioma", collectionResourceRel="idiomas")
 public interface LanguageRepository extends JpaRepository<Language, Integer> {
-
 	@Override
 	@RestResource(exported = false)
 	void deleteById(Integer id);
