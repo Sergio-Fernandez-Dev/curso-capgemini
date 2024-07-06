1a564479848f022a686c301db5ba7681cc104843
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2024-07-06 18:08:24 +0200

    implements microservices

--- catalogo/src/main/java/com/catalogo/CatalogoApplication.java
+++ catalogo/src/main/java/com/catalogo/CatalogoApplication.java
@@ -1,38 +1,63 @@
 package com.catalogo;
 
+import java.util.TreeMap;
+
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import jakarta.transaction.Transactional;
+
 @SpringBootApplication
 @EnableDiscoveryClient
-public class CatalogoApplication implements CommandLineRunner{
-
-
-	public static void main(String[] args) {
-		SpringApplication.run(CatalogoApplication.class, args);
-	}
-	
-	@Override
-	public void run(String... args) throws Exception {
-		System.err.println("Aplicación arrancada...");
-	}
+@EnableFeignClients("com.catalogo.application.proxies")
+@OpenAPIDefinition(
+		 info = @Info(title = "Microservicio: Catálogo", version = "1.0",
+		 description = "Práctica de  microservicios."
+		 ),
+		 externalDocs = @ExternalDocumentation(description = "Documentación", url = "https://github.com/Sergio-Fernandez-Dev/curso-capgemini")
+		)
+public class CatalogoApplication implements CommandLineRunner {
 
 	@Bean
+	public OpenApiCustomizer sortSchemasAlphabetically() {
+	 return openApi -> {
+	 var schemas = openApi.getComponents().getSchemas();
+	 openApi.getComponents().setSchemas(new TreeMap<>(schemas));
+	 };
+	}
+	@Bean 
 	@Primary
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+	public RestTemplate restTemplate(RestTemplateBuilder builder) { // sin balancear
 		return builder.build();
 	}
 
-	@Bean
+	@Bean 
 	@LoadBalanced
 	public RestTemplate restTemplateLB(RestTemplateBuilder builder) {
 		return builder.build();
 	}
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoApplication.class, args);
+	}
+
+
+	@Override
+	@Transactional
+	public void run(String... args) throws Exception {
+		System.err.println("Aplicación arrancada...");
+
+	}
+
 }
--- catalogo/src/main/java/com/catalogo/application/resources/FilmResource.java
+++ catalogo/src/main/java/com/catalogo/application/resources/FilmResource.java
@@ -17,12 +17,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.catalogo.application.proxies.MeGustaProxy;
 import com.catalogo.domains.contracts.services.FilmService;
 import com.catalogo.domains.entities.Category;
 import com.catalogo.domains.entities.Film;
@@ -34,6 +36,10 @@ import com.catalogo.domains.entities.models.FilmShortDTO;
 import com.catalogo.exceptions.BadRequestException;
 import com.catalogo.exceptions.NotFoundException;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 
@@ -152,5 +158,21 @@ public class FilmResource {
 			throws Exception {
 		srv.deleteById(id);
 	}
+	
+
+	@Autowired
+	MeGustaProxy proxy;
+	
+	@Operation(summary = "Enviar un me gusta")
+	@ApiResponse(responseCode = "200", description = "Like enviado")
+	@SecurityRequirement(name = "bearerAuth")
+	@PostMapping(path = "{id}/like")
+	public String like(@Parameter(description = "Identificador de la pelicula", required = true) @PathVariable int id,
+			@Parameter(hidden = true) @RequestHeader(required = false) String authorization) throws Exception {
+		if (authorization == null)
+			return proxy.sendLike(id);
+		return proxy.sendLike(id, authorization);
+	}
+	
 
 }
