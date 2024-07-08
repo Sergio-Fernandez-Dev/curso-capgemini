bb6f695cd3a332c488ab29b21faffe4e82c28ddd
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2024-07-08 08:29:06 +0200

    delete duplicated folder

--- catalogo/catalogo/src/main/java/com/catalogo/domains/contracts/repositories/ActorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.catalogo.domains.contracts.repositories;
-
-import com.catalogo.domains.core.contracts.repositories.ProjectionsAndSpecificationJpaRepository;
-import com.catalogo.domains.entities.Actor;
-
-public interface ActorRepository extends ProjectionsAndSpecificationJpaRepository<Actor, Integer> {
-	
-}
