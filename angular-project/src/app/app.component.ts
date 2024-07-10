5a15e5e5103e1ef260fd1966451d388028e30eac
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2024-07-10 14:13:16 +0200

    create notification component

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -1,23 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SecurityModule } from './security';
-import { LoggerService, MyCoreModule } from '@my/core';
+import { NotificationComponent } from './main/notification/notification.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SecurityModule, MyCoreModule],
+  imports: [RouterOutlet, SecurityModule, NotificationComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title: string = 'angular-project';
-  
-  constructor(log: LoggerService) {
-    log.error('Es un error');
-    log.warn('Es un warn');
-    log.info('Es un info');
-    log.log('Es un log');
-
-  }
 }
