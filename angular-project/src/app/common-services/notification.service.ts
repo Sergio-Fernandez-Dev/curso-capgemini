8e2112899d4ceb6c0569b9beb960e217b372d5c9
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2024-07-10 14:48:01 +0200

    convert service in observable

--- angular-project/src/app/common-services/notification.service.ts
+++ angular-project/src/app/common-services/notification.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { LoggerService } from '@my/core';
+import { Subject } from 'rxjs';
 
 export enum NotificationType {
   error = 'error',
@@ -26,12 +27,15 @@ export class Notification {
 }
 
 @Injectable({ providedIn: 'root' })
-export class NotificationService {
+export class NotificationService implements OnDestroy {
   public readonly NotificationType = NotificationType;
   private listado: Notification[] = [];
-
+  private notificacion$ = new Subject<Notification>();
   constructor(private out: LoggerService) {}
 
+  public get Notificacion() {
+    return this.notificacion$;
+  }
   public get Listado(): Notification[] {
     return Object.assign([], this.listado);
   }
@@ -39,20 +43,20 @@ export class NotificationService {
   public get HayNotificaciones() {
     return this.listado.length > 0;
   }
-
-  public add(msg: string, type: NotificationType = NotificationType.error) {
+  add(msg: string, type: NotificationType = NotificationType.error) {
     if (!msg || msg === '') {
       this.out.error('Falta el mensaje de notificación.');
       return;
     }
-
     const id = this.HayNotificaciones
       ? this.listado[this.listado.length - 1].Id + 1
       : 1;
-
-
     const n = new Notification(id, msg, type);
     this.listado.push(n);
+    this.notificacion$.next(n);
+    if (type === NotificationType.error) {
+      this.out.error(`NOTIFICATION: ${msg}`);
+    }
   }
   public remove(index: number) {
     if (index < 0 || index >= this.listado.length) {
@@ -62,8 +66,12 @@ export class NotificationService {
 
     this.listado.splice(index, 1);
   }
-  
+
   public clear() {
     if (this.HayNotificaciones) this.listado.splice(0);
+  }
+
+  ngOnDestroy(): void {
+    this.notificacion$.complete();
   }
 }
