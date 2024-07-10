4e986e7f5ae61a77f2f7ed1a9781bac256a8bd45
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2024-07-10 14:23:27 +0200

    create demos module

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SecurityModule } from './security';
 import { NotificationComponent } from './main/notification/notification.component';
+import { DemosComponent } from './demos/demos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SecurityModule, NotificationComponent,],
+  imports: [RouterOutlet, SecurityModule, NotificationComponent,DemosComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
