19c9f3efe46247a23bc7ce586044179748a9ab05
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2024-07-15 09:29:58 +0200

    create form example

--- angular-project/src/app/ejemplos/dashboard/dashboard.component.ts
+++ angular-project/src/app/ejemplos/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from 'src/app/main/home/home.component';
 import GraficoSvgComponent from 'src/lib/my-core/components/grafico-svg/grafico-svg.component';
-import { DemosComponent } from '../demos/demos.component';
+import { FormularioComponent } from '../formulario/formulario.component';
 import { NotificationComponent } from "../../main/notification/notification.component";
 import { CommonModule } from '@angular/common';
 import { CalculadoraComponent } from 'src/lib/my-core/components/calculadora/calculadora.component';
@@ -18,7 +18,7 @@ export class DashboardComponent {
   menu = [
     { texto: 'calculadora', icono: '', componente: CalculadoraComponent },
     { texto: 'inicio', icono: '', componente: HomeComponent },
-    { texto: 'demos', icono: '', componente: DemosComponent },
+    { texto: 'formulario', icono: '', componente: FormularioComponent },
     { texto: 'gráfico', icono: '', componente: GraficoSvgComponent },
 
   ];
