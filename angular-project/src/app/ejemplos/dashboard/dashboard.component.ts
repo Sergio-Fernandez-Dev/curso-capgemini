52bb5a966eaaba477254476ecdfe329c41043b60
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2024-07-15 13:48:17 +0200

    add spinner

--- angular-project/src/app/ejemplos/dashboard/dashboard.component.ts
+++ angular-project/src/app/ejemplos/dashboard/dashboard.component.ts
@@ -1,16 +1,18 @@
+
 import { Component } from '@angular/core';
-import { HomeComponent } from 'src/app/main';
+import { AjaxWaitComponent, HomeComponent } from 'src/app/main';
 import { DemosComponent } from '../demos/demos.component';
 import GraficoSvgComponent from 'src/lib/my-core/components/grafico-svg/grafico-svg.component';
 import { NotificationComponent } from "../../main/notification/notification.component";
 import { CommonModule } from '@angular/common';
+
 import { FormularioComponent } from '../formulario/formulario.component';
 import { CalculadoraComponent } from 'src/lib/my-core/components/calculadora/calculadora.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NotificationComponent, CommonModule, ],
+  imports: [NotificationComponent, CommonModule, AjaxWaitComponent, ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
