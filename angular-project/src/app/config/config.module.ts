aa16b64de2fe2102d6748b2ac442a175b98ffd4c
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2024-07-15 14:25:51 +0200

    add routes

--- angular-project/src/app/config/config.module.ts
+++ angular-project/src/app/config/config.module.ts
@@ -4,17 +4,41 @@ import { ConfiguracionComponent } from './configuracion/configuracion.component'
 import { DatosComponent } from './datos/datos.component';
 import { PermisosComponent } from './permisos/permisos.component';
 import { Routes, RouterModule } from '@angular/router';
+import {
+  ContactosListComponent,
+  ContactosAddComponent,
+  ContactosEditComponent,
+  ContactosViewComponent,
+} from '../contactos';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: ConfiguracionComponent },
   { path: 'datos', component: DatosComponent },
   { path: 'permisos', component: PermisosComponent },
+  {
+    path: 'contactos',
+    children: [
+      { path: '', component: ContactosListComponent },
+      { path: 'add', component: ContactosAddComponent },
+      { path: ':id/edit', component: ContactosEditComponent },
+      { path: ':id', component: ContactosViewComponent },
+      { path: ':id/:kk', component: ContactosViewComponent },
+    ],
+  },
 ];
 
 @NgModule({
-    imports: [
-        CommonModule, RouterModule.forChild(routes),
-        ConfiguracionComponent, DatosComponent, PermisosComponent,
-    ]
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ConfiguracionComponent,
+    DatosComponent,
+    PermisosComponent,
+    ContactosListComponent,
+    ContactosAddComponent,
+    ContactosEditComponent,
+    ContactosViewComponent,
+    ContactosViewComponent,
+  ],
 })
-export default class ConfigModule { }
+export default class ConfigModule {}
