1f49350c7776cbc76a84683fffb80e8f7e871f32
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2024-07-15 14:22:03 +0200

    add contacts to navbar

--- angular-project/src/app/ejemplos/dashboard/dashboard.component.ts
+++ angular-project/src/app/ejemplos/dashboard/dashboard.component.ts
@@ -8,6 +8,7 @@ import { CommonModule } from '@angular/common';
 
 import { FormularioComponent } from '../formulario/formulario.component';
 import { CalculadoraComponent } from 'src/lib/my-core/components/calculadora/calculadora.component';
+import { ContactosComponent } from 'src/app/contactos';
 
 @Component({
   selector: 'app-dashboard',
@@ -18,10 +19,11 @@ import { CalculadoraComponent } from 'src/lib/my-core/components/calculadora/cal
 })
 export class DashboardComponent {
   menu = [
+    { texto: 'contactos', icono: 'fa-solid fa-person', componente: ContactosComponent},
+    { texto: 'calculadora', icono: 'fa-solid fa-calculator', componente: CalculadoraComponent },
     { texto: 'formulario', icono: 'fa-solid fa-person-chalkboard', componente: FormularioComponent},
     { texto: 'inicio', icono: 'fa-solid fa-house', componente: HomeComponent },
     { texto: 'demos', icono: 'fa-solid fa-person-chalkboard', componente: DemosComponent},
-    { texto: 'calculadora', icono: 'fa-solid fa-calculator', componente: CalculadoraComponent },
     { texto: 'gráfico', icono: 'fa-solid fa-image', componente: GraficoSvgComponent },
   ]
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
