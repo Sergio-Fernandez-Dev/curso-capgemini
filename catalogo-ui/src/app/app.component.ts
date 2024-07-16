d0acf674b24b40a26fb6c87fb4aaf8d8e563eb63
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2024-07-16 12:35:15 +0200

    implement global services

--- catalogo-ui/src/app/app.component.ts
+++ catalogo-ui/src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  AjaxWaitComponent,
+  HeaderComponent,
+  NotificationComponent,
+  NotificationModalComponent,
+} from './main';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NotificationComponent,
+    NotificationModalComponent,
+    AjaxWaitComponent,
+    HeaderComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'catalogo-ui';
-}
+export class AppComponent {}
--- catalogo-ui/src/app/app.routes.ts
+++ catalogo-ui/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: },
+    { path: 'peliculas/v1', component: },
+    { path: 'peliculas/v1/:id', component: },
+    { path: 'peliculas/v1/:id/categorias', component: },
+    { path: 'peliculas/v1/:id/like', component: },
+    { path: 'peliculas/v1/:id/reparto', component: },
+];
